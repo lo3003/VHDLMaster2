a1b1bc2f4b5af3e948e7f5e396c2eec4d5e86a44
Mathceli <la215918@student.helha.be>
2025-10-21 17:00:11 +0200

    Update hard_seq_generator.vhd

--- Fichiers VHDL/hard_seq_generator.vhd
+++ Fichiers VHDL/hard_seq_generator.vhd
@@ -17,7 +17,6 @@ end entity;
 architecture rtl of hard_seq_generator is
     -- Type du tableau pour la séquence
     type seq_array is array (0 to N_MAX-1) of unsigned(3 downto 0);
-	 signal valid : std_logic;
     
     -- Séquence fixe codée en dur
     constant sequence : seq_array := (
@@ -37,23 +36,21 @@ begin
     -- Process synchrone pour lire la ROM
 	process(clk, reset)
 	begin
-		 if reset = '0' then
+		 if reset = '1' then
 			  -- Initialise les sorties à un état connu
 			  value_o <= (others => '0');
 			  send_valid <= '0';
-			  valid <= '0';
 		 elsif rising_edge(clk) then
 			  -- Si une commande est reçue et que l'index est valide
 			  if send_command = '1' and to_integer(index) < N_MAX then
 					value_o <= sequence(to_integer(index));
-					valid <= '1';
+					send_valid <= '1';
 					
 				-- Valid retourne à 0 après un cycle
-			  elsif valid = '1' then
-					valid <= '0';
+			  else
+					send_valid <= '0';
 			  end if;
 		 end if;
 	end process;
 	
-	send_valid <= valid;
 end architecture;
